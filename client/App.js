1599281bea6cec0eea79db87f96f9bc73c2288b4
Kekoa Wong <kekoawong@macbook-pro.dhcp.nd.edu>
2021-03-10 23:36:02 -0500

    Setup initial navigation

--- client/App.js
+++ client/App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ListScreen from './src/list/listScreen';
+import MapScreen from './src/map/mapScreen';
+import ProfileScreen from './src/profile/profileScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
   return (
-    <View style={styles.container}>
-      <Text>CAMPUS WAYYYZZZZZZ</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="List" component={ListScreen} />
+      <Tab.Screen name="Feed" component={MapScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="List" component={HomeTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
