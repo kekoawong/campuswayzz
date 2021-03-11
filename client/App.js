1fb21274d9f2bbd611d2dbf5a83bb7164a483438
kekoawong <kwong6@nd.edu>
2021-03-11 17:29:12 -0500

    Added tab icons

--- client/App.js
+++ client/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { MaterialIcons } from '@expo/vector-icons';
 import ListScreen from './src/list/listScreen';
 import MapScreen from './src/map/mapScreen';
 import ProfileScreen from './src/profile/profileScreen';
@@ -13,9 +14,36 @@ export default function App() {
     <SafeAreaProvider>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="List" component={ListScreen} />
-          <Tab.Screen name="Feed" component={MapScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
+          <Tab.Screen 
+              name="List" 
+              component={ListScreen}
+              options={{
+                tabBarLabel: 'List',
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialIcons name="list-alt" color={color} size={size} />
+                ),
+              }}
+          />
+          <Tab.Screen 
+              name="Map" 
+              component={MapScreen} 
+              options={{
+                tabBarLabel: 'Map',
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialIcons name="map" color={color} size={size} />
+                ),
+              }}
+          />
+          <Tab.Screen 
+              name="Profile" 
+              component={ProfileScreen} 
+              options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialIcons name="person" color={color} size={size} />
+                ),
+              }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
