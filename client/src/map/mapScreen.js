de7c2ed995b03317b97709614bad17af7a2d8f67
Chase Saca <chasesaca@chases-mbp.dhcp.nd.edu>
2021-03-25 14:23:59 -0400

    markers are working

--- client/src/map/mapScreen.js
+++ client/src/map/mapScreen.js
@@ -1,22 +1,47 @@
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, {useState} from 'react';
+import MapView, { Marker } from 'react-native-maps';
+
 import { createStackNavigator } from '@react-navigation/stack';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
 
 function MainMap() {
+
+  /* initial coordinates that map will be centered on */
+  const initialCoordinates = {
+    latitude: 41.7030,
+    longitude: -86.2390,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }
+
+  /* hard-coded marker coordinates - temporary until middleware works */
+  const arrCoordinates = [
+    {latitude: 41.7030, longitude: -86.2390},
+    {latitude: 41.7035, longitude: -86.2390},
+    {latitude: 41.7025, longitude: -86.2390}
+  ]
+
+  /* eventually, make coordinates and markers a state so that they can RELOAD when the user queries */
+
+  /* state for type of places showing */
+  /*const [locationType, setLocationType] = useState('all');*/
+
+  /* function onClick for marker */
+
   return (
       <View style={styles.container}>
-        <Text>Map Screen</Text>
         <MapView style={styles.map} 
-          initialRegion={{
-            latitude: 41.7030,
-            longitude: -86.2390,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
+          initialRegion={initialCoordinates}
           showsBuildings={true}
           loadingEnabled={true}
-        />
+        >
+          {arrCoordinates.map((coord, index) => (
+            <Marker 
+              key={index}
+              coordinate={coord}
+            />
+          ))}
+        </MapView>
       </View>
     );
 }
