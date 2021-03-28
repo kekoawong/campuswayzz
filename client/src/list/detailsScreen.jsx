65aadea438d5b0101480e1c21987904c03ed0028
kekoawong <kwong6@nd.edu>
2021-03-27 23:09:15 -0400

    Altered listings

--- client/src/list/detailsScreen.jsx
+++ client/src/list/detailsScreen.jsx
@@ -3,7 +3,7 @@ import { Button, View, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 
-export default function DetailsScreen() {
+export default function DetailsScreen({item}) {
     const navigation = useNavigation();
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
