324d193a54cd8e07accda68a1e7587bafad023d7
kekoawong <kwong6@nd.edu>
2021-03-28 12:24:07 -0400

    deleted keyboard navigation

--- client/src/list/detailsScreen.jsx
+++ client/src/list/detailsScreen.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Button, View, Text, StyleSheet, ScrollView } from 'react-native';
 import { TextInput } from 'react-native-paper';
-import { KeyboardAccessoryNavigation } from 'react-native-keyboard-accessory';
 
 
 export default function DetailsScreen({ route, navigation }) {
@@ -11,26 +10,20 @@ export default function DetailsScreen({ route, navigation }) {
     // get item from route params
     const { item } = route.params;
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.details}>
-            <Text>{item.title}</Text>
-            <Button
-              title="Go to Details... again"
-              onPress={() => console.log(route)}
-            />
-            <TextInput
-              label="Comment"
-              value={text}
-              onChangeText={text => setText(text)}
-            />
-        </ScrollView>
-        <KeyboardAccessoryNavigation
-          avoidKeyboard
-          inSafeAreaView
-          nextHidden
-          previousHidden
-        />
-      </View>
+        <View style={styles.container}>
+          <ScrollView style={styles.details}>
+              <Text>{item.title}</Text>
+              <Button
+                title="Go to Details... again"
+                onPress={() => console.log(route)}
+              />
+              <TextInput
+                label="Comment"
+                value={text}
+                onChangeText={text => setText(text)}
+              />
+          </ScrollView>
+        </View>
     );
 }
 
