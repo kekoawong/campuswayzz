885d08d4ad35b1ab3866cdb21cb9738ecc12e212
Chase Saca <chasesaca@Chases-MacBook-Pro.local>
2021-03-28 01:17:29 -0400

    custom markers are working

--- client/src/map/mapScreen.js
+++ client/src/map/mapScreen.js
@@ -1,12 +1,11 @@
 import React, {useState} from 'react';
 import MapView, { 
   Marker,
-  Callout,
-  CalloutSubview 
+  Callout
 } from 'react-native-maps';
 
 import { createStackNavigator } from '@react-navigation/stack';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Button } from 'react-native';
 
 function MainMap() {
 
@@ -19,10 +18,10 @@ function MainMap() {
   }
 
   /* hard-coded marker coordinates - temporary until middleware works */
-  const arrCoordinates = [
-    {latitude: 41.7030, longitude: -86.2390},
-    {latitude: 41.7035, longitude: -86.2390},
-    {latitude: 41.7025, longitude: -86.2390}
+  const arrLocations = [
+    {type:"all", latlng: {latitude: 41.7030, longitude: -86.2390}, name:"Main Building", address:"Main Building,\nNotre Dame, IN 46556", description:"Heart of campus."},
+    {type:"restaurant", latlng: {latitude: 41.6984, longitude: -86.2339}, name:"Notre Dame Stadium", address:"2010 Moose Krause Circle,\nNotre Dame, IN 46556", description: "Where the Irish win!"},
+    {type:"study", latlng: {latitude: 41.7025, longitude: -86.2341}, name:"Hesburgh Library", address:"284 Hesburgh Library,\nNotre Dame, IN 46556", description:"A great place to study."}
   ]
 
   /* eventually, make coordinates and markers a state so that they can RELOAD when the user queries */
@@ -30,20 +29,25 @@ function MainMap() {
 
   return (
       <View style={styles.container}>
+        <Button title="Test"></Button>
         <MapView style={styles.map} 
           initialRegion={initialCoordinates}
           showsBuildings={true}
           loadingEnabled={true}
         >
-          {arrCoordinates.map((coord, index) => (
+          {arrLocations.map((marker, index) => (
             <Marker 
               key={index}
-              coordinate={coord}
+              coordinate={marker.latlng}
+              /*title={marker.name}
+              description={marker.address}*/
               /*onPress={() => handleMarkerPress(index) }*/
             >
               <Callout style={styles.plainView}>
                 <View>
-                  <Text> hopefully this works!</Text>
+                  <Text style={{ fontWeight: 'bold' }}>{marker.name}</Text>
+                  <Text>{marker.address}</Text>
+                  <Text>{marker.description}</Text>
                 </View>
               </Callout>
             </Marker>
