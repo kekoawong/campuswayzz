5a100c27f8e0f559f872c9cab1f5859172adf467
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2021-03-29 13:08:55 -0400

    get API calls configured with proper url

--- client/src/utils/map.util.js
+++ client/src/utils/map.util.js
@@ -1,7 +1,17 @@
 const https = require('https');
 
+/* make API call to server to get locations for a given type */
 function getLocationsForType(type){
-    return fetch('http://localhost:5000/api/locations/' + type)
+    return fetch('http://db.cse.nd.edu:5002/api/locations/' + type)
+    .then(res => res.json())
+    .then(json => {
+        return json;
+    });
+}
+
+/* make API call to server to get locations for ALL locations */
+function getLocations(){
+    return fetch('http://db.cse.nd.edu:5002/api/locations')
     .then(res => res.json())
     .then(json => {
         return json;
@@ -9,5 +19,6 @@ function getLocationsForType(type){
 }
 
 export default {
-    getLocationsForType
+    getLocationsForType,
+    getLocations
 }
