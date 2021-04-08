7dd24c196fc16f33e2d925e7bb75f49cdf4ee9a8
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2021-04-08 13:54:54 -0400

    add debug statements

--- client/src/utils/map.util.js
+++ client/src/utils/map.util.js
@@ -11,9 +11,12 @@ function getLocationsForType(type){
 
 /* make API call to server to get locations for ALL locations */
 function getLocations(){
+    console.log("fetch all locations");
     return fetch('http://db.cse.nd.edu:5002/api/locations')
     .then(res => res.json())
     .then(json => {
+        console.log("resulting json:");
+        console.log(json);
         return json;
     });
 }
