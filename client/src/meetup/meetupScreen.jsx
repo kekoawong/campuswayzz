84f7285794c07ab859a0939b2bbd125a5d51a8af
kekoawong <kwong6@nd.edu>
2021-04-21 14:53:21 -0400

    Inital create meetup

--- client/src/meetup/meetupScreen.jsx
+++ client/src/meetup/meetupScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback, Text, Button, Keyboard, TextInput } from 'react-native';
 import { FAB } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 
@@ -8,22 +8,52 @@ export default function MeetupScreen() {
     const navigation = useNavigation();
       
     return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.container}
+            >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.inner}>
+                <Text style={styles.header}>Header</Text>
+                <TextInput placeholder="Username" style={styles.textInput} />
+                <View style={styles.btnContainer}>
+                    <Button title="Submit" onPress={() => null} />
+                </View>
+                </View>
+            </TouchableWithoutFeedback>
             <FAB
                 style={styles.fab}
                 label='Start Meetup'
                 icon="walk"
                 onPress={() => navigation.navigate('MyModal')}
             />
-        </View>
+        </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      overflow: 'scroll'
+      backgroundColor: '#fff'
+    },
+    inner: {
+        padding: 24,
+        flex: 1,
+        justifyContent: "space-around"
+    },
+    header: {
+        fontSize: 36,
+        marginBottom: 48
+     },
+    textInput: {
+        height: 40,
+        borderColor: "#000000",
+        borderBottomWidth: 1,
+        marginBottom: 36
+    },
+    btnContainer: {
+        backgroundColor: "white",
+        marginTop: 12
     },
     fab: {
       alignSelf: 'center',
